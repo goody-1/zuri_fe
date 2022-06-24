c8ba07c34042a3550dbeaee3e1db0ac5fd4f50f5
Goodness <atundeg@gmail.com>
2022-06-24 11:47:18 +0100

    added alert

--- js/intro.js
+++ js/intro.js
@@ -2,4 +2,5 @@ let my_name = "Goodness";
 let height = 5.9;
 let country = "Nigeria";
 
-console.log(`My name is ${my_name}, I am ${height}ft tall and my country is ${country}`)
+output = `My name is ${my_name}, I am ${height}ft tall and my country is ${country}`
+console.log(output); alert(output)
